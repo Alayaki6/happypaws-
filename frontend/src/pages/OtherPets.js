fe64ecae397d7e8680b02d1b30e9477c9ec606ba
Alayaki6 <150488587+Alayaki6@users.noreply.github.com>
2026-08-24 19:27:20 +0100

    Create OtherPets.js

---
+++ frontend/src/pages/OtherPets.js
@@ -0,0 +1,13 @@
+function OtherPets() {
+  return (
+    <main>
+      <h1>Other Pets</h1>
+      <p>
+        Explore rabbits, birds, hamsters, guinea pigs, fish, reptiles,
+        and other permitted pets.
+      </p>
+    </main>
+  );
+}
+
+export default OtherPets;
