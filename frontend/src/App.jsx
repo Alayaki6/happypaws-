d47cde3f77ea7db217c6fbfc894ba0dc00a5e237
Alayaki6 <150488587+Alayaki6@users.noreply.github.com>
2026-08-24 19:20:49 +0100

    Create App.jsx
    Create HappyPaws application entry

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./styles/index.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>HappyPaws</h1>
+      <p>Find your perfect companion.</p>
+    </div>
+  );
+}
+
+export default App;
