eb9005b7145aa1a04533ec819487335ad2477afb
Alayaki6 <150488587+Alayaki6@users.noreply.github.com>
2026-08-24 19:32:14 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Dogs from "./pages/Dogs";
+import Cats from "./pages/Cats";
+import OtherPets from "./pages/OtherPets";
+import PetDetails from "./pages/PetDetails";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+
 import "./styles/index.css";
 
 function App() {
   return (
-    <div className="app">
-      <h1>HappyPaws</h1>
-      <p>Find your perfect companion.</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dogs" element={<Dogs />} />
+        <Route path="/cats" element={<Cats />} />
+        <Route path="/other-pets" element={<OtherPets />} />
+        <Route path="/pets/:petId" element={<PetDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
