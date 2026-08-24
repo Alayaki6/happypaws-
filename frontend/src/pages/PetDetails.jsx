a77b8851d1c93ef7662e1d4079670d857097e550
Alayaki6 <150488587+Alayaki6@users.noreply.github.com>
2026-08-24 19:28:01 +0100

    Create PetDetails.jsx

---
+++ frontend/src/pages/PetDetails.jsx
@@ -0,0 +1,10 @@
+function PetDetails() {
+  return (
+    <main>
+      <h1>Pet Details</h1>
+      <p>Detailed information about a selected pet will appear here.</p>
+    </main>
+  );
+}
+
+export default PetDetails;
